ec39e752e39cf858f69ac4326c0c0bdc7612d78b
bettez1998 <51047064+bettez1998@users.noreply.github.com>
2022-07-19 16:30:42 +0800

    wip

--- New Unity Project/Assets/Scripts/AnimControl.cs
+++ New Unity Project/Assets/Scripts/AnimControl.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class AnimControl : MonoBehaviour
 {
-    // Start is called before the first frame update
     private Animation scissorsanimation;
     void Start()
     {
         scissorsanimation = this.gameObject.GetComponent<Animation>();
     }
 
-    // Update is called once per frame
     public void PlayAnim()
     {
         scissorsanimation.Play();
