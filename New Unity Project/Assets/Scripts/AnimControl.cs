23c49a4f86f39d13e2c2e2d379c62b031251f09f
bettez1998 <51047064+bettez1998@users.noreply.github.com>
2022-07-27 17:31:11 +0800

    stage2_timer_wip

--- New Unity Project/Assets/Scripts/AnimControl.cs
+++ New Unity Project/Assets/Scripts/AnimControl.cs
@@ -6,15 +6,21 @@ using UnityEngine;
 public class AnimControl : MonoBehaviour
 {
     public Animator scissors00;
+    public int cuttimes;
+
     //private Animation scissorsanimation;
     void Start()
     {
         //scissorsanimation = this.gameObject.GetComponent<Animation>();
+        cuttimes = 0;
     }
 
     public void PlayAnim()
     {
         scissors00.Play("scissors00",0,0f);
         //scissorsanimation.Play();
+        cuttimes += 1;
+        print(cuttimes);
     }
+    
 }
